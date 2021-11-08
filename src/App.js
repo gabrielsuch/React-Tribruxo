0d406faa3e281026b5880fbc5879d0b36c501bfa
Gabriel <gabriel.such@hotmail.com>
2021-11-08 12:16:11 -0300

    Refactored Code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import { useState, useEffect } from 'react';
 
 import Random from "./components/RandomCharacter"
+import Loading from "./components/Loading"
 import './App.css';
 
 function App() {
 
   const [characters, setCharacters] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  console.log(isLoading)
 
   useEffect(() => {
     fetch("https://hp-api.herokuapp.com/api/characters/students")
     .then((response) => response.json())
-    .then((response) => setCharacters(...characters, response))
+    .then((response) => {
+      setCharacters(...characters, response)
+      setIsLoading(false)
+    })
     .catch((err) => console.log(err))
   }, [])
 
   return (
     <div className="App">
-      <Random characters={characters}/>
+      {
+        isLoading ? <Loading/>
+        : 
+        <Random characters={characters}/>
+      }
     </div>
   );
 }
---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+const Loading = () => {
+    return (
+        <div className="loadingMessage">
+            <h1>Carregando...</h1>
+        </div>
+    )
+}
+
+export default Loading
